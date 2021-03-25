cc6b4ea83c3e7d2a47dfc989f9ba5e7b98400553
Ester Ribeiro dos Santos <ester.santos@teadit.com>
2021-03-24 23:43:29 -0300

    Gherkin com specflow e xunit

---
+++ CalculoFinanceiro.Tests/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,53 @@
+﻿using JurosCompostos.Main;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace CalculoFinanceiro.Tests.Steps
+{
+    [Binding]
+    public sealed class CalculoFinanceiroSteps
+    {
+        private double _valorEmprestimo;
+        private double _taxa;
+        private int _meses;
+        private double _valorFinalPeriodo;
+
+        //private readonly ScenarioContext _scenarioContext;
+
+        public CalculoFinanceiroSteps(ScenarioContext scenarioContext)
+        {
+            //_scenarioContext = scenarioContext;
+        }
+
+        [Given("que o valor do emprestimo foi R\\$ (.*)")]
+        public void CarregarValorEmprestimo(double valorEmprestimo)
+        {
+
+            _valorEmprestimo = valorEmprestimo;
+        }
+
+        [Given("foi definida a taxa de (.*) mensais")]
+        public void CarregarTaxa(double taxa)
+        {
+            _taxa = taxa;
+        }
+
+        [Given("em um periodo de (.*) meses")]
+        public void CarregarMeses(int meses)
+        {
+            _meses = meses;
+        }
+
+        [When("eu solicitar o valor ao final do periodo")]
+        public void ProcessarCalculo()
+        {        
+           _valorFinalPeriodo = CalculoFinanc.CalcularValorComJurosCompostos(_valorEmprestimo, _meses, _valorFinalPeriodo);
+        }
+
+        [Then("o valor total a ser pago sera R\\$ (.*)")]
+        public void VerificarReultado(double valorFinalPeriodo)
+        {
+            Assert.Equal(valorFinalPeriodo, _valorFinalPeriodo);
+        }
+    }
+}
---
+++ JurosCompostos.Main/CalculoFinanc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JurosCompostos.Main
+{
+    public static class CalculoFinanc
+    {
+        public static double CalcularValorComJurosCompostos(double valorEmprestimo, int numMeses, double taxa)
+        {
+            return double.MinValue;
+        }
+    }
+}
