44f3b48127e8a587154007b40ed830d25dd47c2e
Ester Ribeiro dos Santos <ester.santos@teadit.com>
2021-03-28 22:34:07 -0300

    Exemplo FluentAssertions

---
+++ CaixaEletronico.Test/FluentAssertionsTest.cs
@@ -0,0 +1,33 @@
+﻿using CaixaEletronico.Domain;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CaixaEletronico.Test
+{
+    /// <summary>
+    /// Usando FluentAssertions, para assegurar valores de atributos (se foi alterado ou está incorreto, nulo)
+    /// Vantagem: A programação fica mais funcional e aparentemente limpa
+    /// </summary>
+    public class FluentAssertionsTest
+    {
+        [Fact(DisplayName ="Valores do modelo cedula estão corretos")]
+        public void ValoresModeloCedulaEstaoCorretos()
+        {
+            Cedula.Cem.Should().Be(100);
+            Cedula.Cinquenta.Should().BeOfType(typeof(int)).And.Be(50);
+            Cedula.Vinte.Should().BeOfType(typeof(int)).And.Be(20);
+        }
+
+        [Fact(DisplayName ="Intervalos de valores do modelo cedula estao corretos")]
+        public void IntervalosValoresModeloCedulasCorretos()
+        {
+            Cedula.Cinquenta.Should().BeLessThan(Cedula.Cem);
+            Cedula.Dez.Should().BeLessThan(Cedula.Vinte);
+        }
+    }
+}
